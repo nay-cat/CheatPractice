6e591266fb681c899918578f5c57a068ed8a6b1f
Nay <63517637+nay-cat@users.noreply.github.com>
2023-10-18 22:46:21 +0200

    Add files via upload

---
+++ Compiler/FakeCheat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Practice Autoclick");
+        Console.WriteLine("20 cps | Esto es un autoclick no funcional");
+        Console.WriteLine("hecho para probar ss");
+        Thread.Sleep(8000);
+    }
+}
